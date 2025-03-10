81704680ccf731e8c87171044b0781055c7d15e2
StevenRey1 <js.reyh1@uniandes.edu.co>
2025-03-10 14:03:16 -0500

    first commit

---
+++ app/src/main/java/com/example/runoclock/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.runoclock
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.runoclock.ui.theme.RunOclockTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                // Create a NavHostController
+                val navController = rememberNavController()
+                AppNavigation(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RunOclockTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/runoclock/ui/theme/Color.kt
@@ -0,0 +1,48 @@
+package com.example.runoclock.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Color Swatch
+val primaryColor_0 = Color(0xFFE66210)
+val primaryColor_10 = Color(0xFFED681C)
+val primaryColor_20 = Color(0xFFF46F26)
+val primaryColor_30 = Color(0xFFFB752F)
+val primaryColor_40 = Color(0xFFFF7E3E)
+val primaryColor_50 = Color(0xFFFF884E)
+val primaryColor_60 = Color(0xFFFF905C)
+val primaryColor_70 = Color(0xFFFF996A)
+val primaryColor_80 = Color(0xFFFFA177)
+val primaryColor_90 = Color(0xFFFFB99A)
+val primaryColor_95 = Color(0xFFFFC9B2)
+val primaryColor_99 = Color(0xFFFFD9C8)
+val primaryColor_100 = Color(0xFFFFF7F4)
+
+// Secondary Color Swatch
+val secondaryColor_0 = Color(0xFF2110E6)
+val secondaryColor_10 = Color(0xFF262BF4)
+val secondaryColor_20 = Color(0xFF2C41FF)
+val secondaryColor_30 = Color(0xFF3655FF)
+val secondaryColor_40 = Color(0xFF3B5EFF)
+val secondaryColor_50 = Color(0xFF4266FF)
+val secondaryColor_60 = Color(0xFF6C91FF)
+val secondaryColor_70 = Color(0xFF7B9EFF)
+val secondaryColor_80 = Color(0xFF8BAAFF)
+val secondaryColor_90 = Color(0xFF9BB6FF)
+val secondaryColor_95 = Color(0xFFABC3FF)
+val secondaryColor_99 = Color(0xFFDDE7FF)
+val secondaryColor_100 = Color(0xFFFFFFFF)
+
+// Tertiary Color Swatch
+val tertiaryColor_0 = Color(0xFF915B3A)
+val tertiaryColor_10 = Color(0xFF976140)
+val tertiaryColor_20 = Color(0xFF9E6745)
+val tertiaryColor_30 = Color(0xFFA46C4B)
+val tertiaryColor_40 = Color(0xFFB77E5C)
+val tertiaryColor_50 = Color(0xFFC48A68)
+val tertiaryColor_60 = Color(0xFFD19774)
+val tertiaryColor_70 = Color(0xFFDFA380)
+val tertiaryColor_80 = Color(0xFFECB08C)
+val tertiaryColor_90 = Color(0xFFFABC99)
+val tertiaryColor_95 = Color(0xFFFFCBAD)
+val tertiaryColor_99 = Color(0xFFFFDAC5)
+val tertiaryColor_100 = Color(0xFFFFF1E8)
